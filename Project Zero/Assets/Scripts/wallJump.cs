27af88f84a0494f1efa538bd3d5551c4078dca3c
Agustin Luis <AOLuis@student.fullsail.edu>
2022-09-12 15:33:31 -0400

    Audio for walljump

--- Project Zero/Assets/Scripts/wallJump.cs
+++ Project Zero/Assets/Scripts/wallJump.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class wallJump : MonoBehaviour
 {
+    [SerializeField] AudioSource aud;
+    [SerializeField] AudioClip cl;
+    [SerializeField] float vol;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            aud.PlayOneShot(cl, vol);
             gameManager.instance.playerScript.gravity = gameManager.instance.playerScript.gravity * (float)0.5;
             gameManager.instance.playerScript.jumpsMax = gameManager.instance.playerScript.jumpsMax + 1;
             gameManager.instance.playerScript.jumpsMax = gameManager.instance.playerScript.timesJumped + 1;
