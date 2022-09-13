50a778ed03f8fa940b12b0e063f2e5d3ea62ea8c
Martin Alcantar <Martin.Alcantar.Pena@gmail.com>
2022-09-13 17:01:39 -0400

    Spikes Death Sound
    ahhhhhhhhhhhhhhhhhhhhhhhhhhh

--- Project Zero/Assets/Scripts/spikeTrigger.cs
+++ Project Zero/Assets/Scripts/spikeTrigger.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class spikeTrigger : MonoBehaviour
 {
+
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioSource trigger;
+    [SerializeField] float volume;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            trigger.PlayOneShot(death, volume);
             gameManager.instance.playerScript.death();
+
         }
     }
 }
