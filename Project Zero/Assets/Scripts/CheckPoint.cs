88dd25a468d81b7e39ddc4780b4b574721fb453b
Martin Alcantar <Martin.Alcantar.Pena@gmail.com>
2022-09-17 22:56:22 -0400

    change on checkpoints and minifix on lasers

--- Project Zero/Assets/Scripts/CheckPoint.cs
+++ Project Zero/Assets/Scripts/CheckPoint.cs
@@ -1,14 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CheckPoint : MonoBehaviour
 {
+    [SerializeField] AudioClip checkpoint;
+    [SerializeField] AudioSource trigger;
+    [SerializeField] float volume;
+    [SerializeField] TextMeshProUGUI text;
+    float delay = 4;
+
+    private void Awake()
+    {
+        text.enabled = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            StartCoroutine(ShowCheckPoint());
+            trigger.PlayOneShot(checkpoint, volume);
             gameManager.instance.playerSpawnPos.transform.position = transform.position;
         }
+    }
+
+    IEnumerator ShowCheckPoint() { 
+        text.enabled = true;
+        yield return new WaitForSeconds(delay);
+        text.enabled = false;
     }
 }
